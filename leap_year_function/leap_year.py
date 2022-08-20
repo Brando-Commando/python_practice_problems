b711a37c896f0f6f4aefe4a11ecb642c617fc225
Brando-Commando <brandon.t.599@gmail.com>
2022-08-19 18:21:40 -0600

    readme added

--- leap_year_function/leap_year.py
+++ leap_year_function/leap_year.py
@@ -4,6 +4,8 @@ if __name__ == '__main__':
 def is_leap(year):
     leap = False
     
+    # This was an attempt to combine tree if statements into a single line statement, it was unsuccessful and would 
+    # result in 2/3 tests pass 
     '''
     if ((year % 4) == 0) or ((year % 400) == 0) and not ((year % 100) == 0):
         leap = True
